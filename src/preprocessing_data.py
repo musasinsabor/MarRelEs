08da77a586eb91c9b643b3bcb61490af9b1e3d1f
musasinsabor <genesismontenegro07@gmail.com>
2024-04-08 14:32:54 +0200

    preprocessing data script

--- src/preprocessing_data.py
+++ src/preprocessing_data.py
@@ -2,6 +2,7 @@ import re
 import pandas as pd
 import argparse
 import glob
+import json
 
 
 def elements_extractor(column):
@@ -55,7 +56,22 @@ def field_extractor(column):
     return fields
 
 
-def data_format(file_path):
+def token_filter(df, column):
+    matched_indices = []
+    # Iteration in the df rows
+    for index, row in df.iterrows():
+        # Use the regex
+        reg = re.search(r"\bno\b", str(row[column]))
+        # get the index from matched rows
+        if not reg:
+            matched_indices.append(index)
+
+    # df filter
+    filtered_df = df.loc[matched_indices]
+    return filtered_df
+
+
+def data_format(file_path, cm_types_path):
     """
     Transform a file got from Brawnet with data from IULA to a df
     :param file_path: path with the file
@@ -69,6 +85,9 @@ def data_format(file_path):
     # Open the file and read its contents
     with open(file_path, "r", encoding="utf-8") as file:
         lines = file.readlines()
+
+    with open(cm_types_path) as file2:
+        d = json.load(file2)
 
     # Initialize lists to store extracted information
     col1 = []
@@ -93,6 +112,9 @@ def data_format(file_path):
     right_words, right_tags = elements_extractor(col5)
     all_context_tokens = []
     all_sentence_tokens = []
+    det_presence = []
+    cm_type = []
+    spec_lang = [False if field == "general" else True for field in fields]
     for i in range(0, len(right_words)):
         context = (
             " ".join(left_words[i])
@@ -102,16 +124,23 @@ def data_format(file_path):
         target_sentence = " ".join(target_words[i])
         all_context_tokens.append(context)
         all_sentence_tokens.append(target_sentence)
-
+        dets = re.findall(r"'(A\w+)|(E6\w+)'", str(target_tags[i]))
+        dets_result = [match[0] or match[1] for match in dets]
+        det_presence.append(dets_result)
+        type = d[cm]
+        cm_type.append(type)
     # Create a DataFrame from the extracted information
     data = {
         "cm": cm,
+        "type": cm_type,
         "field": fields,
         "text_id": col2,
         "target_tokens": target_words,
         "target_tags": target_tags,
         "sentence": all_sentence_tokens,
         "context": all_context_tokens,
+        "det": det_presence,
+        "specialized_lang": spec_lang,
     }
     df = pd.DataFrame(data)
     return df
@@ -121,15 +150,19 @@ if __name__ == "__main__":
     # args parser config
     xparser = argparse.ArgumentParser(description="Process txt files in a directory.")
     xparser.add_argument("directory", help="Path to the directory containing txt files")
+    xparser.add_argument("cm_types", help="Path to the file containing cm type parser")
     # args parser
     args = xparser.parse_args()
 
     dirs = glob.glob(f"{args.directory}/*")
     all_data = []
     for path in dirs:
-        test = data_format(path)
+        test = data_format(path, args.cm_types)
         all_data.append(test)
     combined_df = pd.concat(all_data, ignore_index=True)
-    combined_df_sorted = combined_df.sort_values(by='cm')
-    combined_df_sorted.to_csv('data/combined_data.csv', index=False)
-
+    combined_df_sorted = combined_df.sort_values(by="cm")
+    combined_df_sorted.to_csv("data/combined_data.csv", index=False)
+    print("all data phrases extracted", len(combined_df_sorted))
+    neg_filter = token_filter(combined_df_sorted, "sentence")
+    neg_filter.to_csv("data/combined_data_filtered.csv", index=False)
+    print("data without neg", len(neg_filter))
